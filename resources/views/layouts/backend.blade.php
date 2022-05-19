c959a2079fd6106bc5d613cff433f6265b8d6a4d
Peter Snoek <petersnoek@davinci.nl>
2022-05-20 00:15:11 +0200

    created backend view using oneui builtin datatables. visit /contacts page to see how it looks.

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -26,8 +26,6 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap">
   <link rel="stylesheet" id="css-main" href="{{ mix('/css/oneui.css') }}">
 
-  <!-- You can include a specific file from public/css/themes/ folder to alter the default color theme of the template. eg: -->
-  <!-- <link rel="stylesheet" id="css-theme" href="{{ mix('/css/themes/amethyst.css') }}"> -->
   @yield('css_after')
 
   <!-- Scripts -->
@@ -38,234 +36,53 @@
 
 <body>
   <!-- Page Container -->
-  <!--
-    Available classes for #page-container:
-
-    GENERIC
-
-      'remember-theme'                            Remembers active color theme and dark mode between pages using localStorage when set through
-                                                  - Theme helper buttons [data-toggle="theme"],
-                                                  - Layout helper buttons [data-toggle="layout" data-action="dark_mode_[on/off/toggle]"]
-                                                  - ..and/or One.layout('dark_mode_[on/off/toggle]')
-
-    SIDEBAR & SIDE OVERLAY
-
-      'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
-      'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
-      'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
-      'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
-      'sidebar-dark'                              Dark themed sidebar
-
-      'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
-      'side-overlay-o'                            Visible Side Overlay by default
-
-      'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
-
-      'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
-
-    HEADER
-
-      ''                                          Static Header if no class is added
-      'page-header-fixed'                         Fixed Header
-
-    HEADER STYLE
-
-      ''                                          Light themed Header
-      'page-header-dark'                          Dark themed Header
-
-    MAIN CONTENT LAYOUT
-
-      ''                                          Full width Main Content if no class is added
-      'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
-      'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
-
-    DARK MODE
-
-      'sidebar-dark page-header-dark dark-mode'   Enable dark mode (light sidebar/header is not supported with dark mode)
-    -->
   <div id="page-container" class="sidebar-o enable-page-overlay sidebar-dark side-scroll page-header-fixed main-content-narrow">
-    <!-- Side Overlay-->
-    <aside id="side-overlay" class="fs-sm">
-      <!-- Side Header -->
-      <div class="content-header border-bottom">
-        <!-- User Avatar -->
-        <a class="img-link me-1" href="javascript:void(0)">
-          <img class="img-avatar img-avatar32" src="{{ asset('media/avatars/avatar10.jpg') }}" alt="">
-        </a>
-        <!-- END User Avatar -->
 
-        <!-- User Info -->
-        <div class="ms-2">
-          <a class="text-dark fw-semibold fs-sm" href="javascript:void(0)">John Smith</a>
-        </div>
-        <!-- END User Info -->
-
-        <!-- Close Side Overlay -->
-        <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-        <a class="ms-auto btn btn-sm btn-alt-danger" href="javascript:void(0)" data-toggle="layout" data-action="side_overlay_close">
-          <i class="fa fa-fw fa-times"></i>
-        </a>
-        <!-- END Close Side Overlay -->
-      </div>
-      <!-- END Side Header -->
-
-      <!-- Side Content -->
-      <div class="content-side">
-        <p>
-          Content..
-        </p>
-      </div>
-      <!-- END Side Content -->
-    </aside>
-    <!-- END Side Overlay -->
-
-    <!-- Sidebar -->
-    <!--
-        Sidebar Mini Mode - Display Helper classes
-
-        Adding 'smini-hide' class to an element will make it invisible (opacity: 0) when the sidebar is in mini mode
-        Adding 'smini-show' class to an element will make it visible (opacity: 1) when the sidebar is in mini mode
-            If you would like to disable the transition animation, make sure to also add the 'no-transition' class to your element
-
-        Adding 'smini-hidden' to an element will hide it when the sidebar is in mini mode
-        Adding 'smini-visible' to an element will show it (display: inline-block) only when the sidebar is in mini mode
-        Adding 'smini-visible-block' to an element will show it (display: block) only when the sidebar is in mini mode
-    -->
+  <!-- Sidebar -->
     <nav id="sidebar" aria-label="Main Navigation">
-      <!-- Side Header -->
-      <div class="content-header">
-        <!-- Logo -->
-        <a class="font-semibold text-dual" href="/">
-          <span class="smini-visible">
-            <i class="fa fa-circle-notch text-primary"></i>
-          </span>
-          <span class="smini-hide fs-5 tracking-wider">One<span class="fw-normal">UI</span></span>
-        </a>
-        <!-- END Logo -->
 
-        <!-- Extra -->
-        <div>
-          <!-- Dark Mode -->
-          <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-          <a class="btn btn-sm btn-alt-secondary" data-toggle="layout" data-action="dark_mode_toggle" href="javascript:void(0)">
-            <i class="far fa-moon"></i>
-          </a>
-          <!-- END Dark Mode -->
 
-          <!-- Options -->
-          <div class="dropdown d-inline-block ms-1">
-            <a class="btn btn-sm btn-alt-secondary" id="sidebar-themes-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
-              <i class="far fa-circle"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-end fs-sm smini-hide border-0" aria-labelledby="sidebar-themes-dropdown">
-              <!-- Color Themes -->
-              <!-- Layout API, functionality initialized in Template._uiHandleTheme() -->
-              <a class="dropdown-item d-flex align-items-center justify-content-between font-medium" data-toggle="theme" data-theme="default" href="#">
-                <span>Default</span>
-                <i class="fa fa-circle text-default"></i>
-              </a>
-              <a class="dropdown-item d-flex align-items-center justify-content-between font-medium" data-toggle="theme" data-theme="{{ mix('css/themes/amethyst.css') }}" href="#">
-                <span>Amethyst</span>
-                <i class="fa fa-circle text-amethyst"></i>
-              </a>
-              <a class="dropdown-item d-flex align-items-center justify-content-between font-medium" data-toggle="theme" data-theme="{{ mix('css/themes/city.css') }}" href="#">
-                <span>City</span>
-                <i class="fa fa-circle text-city"></i>
-              </a>
-              <a class="dropdown-item d-flex align-items-center justify-content-between font-medium" data-toggle="theme" data-theme="{{ mix('css/themes/flat.css') }}" href="#">
-                <span>Flat</span>
-                <i class="fa fa-circle text-flat"></i>
-              </a>
-              <a class="dropdown-item d-flex align-items-center justify-content-between font-medium" data-toggle="theme" data-theme="{{ mix('css/themes/modern.css') }}" href="#">
-                <span>Modern</span>
-                <i class="fa fa-circle text-modern"></i>
-              </a>
-              <a class="dropdown-item d-flex align-items-center justify-content-between font-medium" data-toggle="theme" data-theme="{{ mix('css/themes/smooth.css') }}" href="#">
-                <span>Smooth</span>
-                <i class="fa fa-circle text-smooth"></i>
-              </a>
-              <!-- END Color Themes -->
 
-              <div class="dropdown-divider"></div>
 
-              <!-- Sidebar Styles -->
-              <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-              <a class="dropdown-item fw-medium" data-toggle="layout" data-action="sidebar_style_light" href="javascript:void(0)">
-                <span>Sidebar Light</span>
-              </a>
-              <a class="dropdown-item fw-medium" data-toggle="layout" data-action="sidebar_style_dark" href="javascript:void(0)">
-                <span>Sidebar Dark</span>
-              </a>
-              <!-- END Sidebar Styles -->
-
-              <div class="dropdown-divider"></div>
-
-              <!-- Header Styles -->
-              <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-              <a class="dropdown-item fw-medium" data-toggle="layout" data-action="header_style_light" href="javascript:void(0)">
-                <span>Header Light</span>
-              </a>
-              <a class="dropdown-item fw-medium" data-toggle="layout" data-action="header_style_dark" href="javascript:void(0)">
-                <span>Header Dark</span>
-              </a>
-              <!-- END Header Styles -->
-            </div>
-          </div>
-          <!-- END Options -->
-
-          <!-- Close Sidebar, Visible only on mobile screens -->
-          <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-          <a class="d-lg-none btn btn-sm btn-alt-secondary ms-1" data-toggle="layout" data-action="sidebar_close" href="javascript:void(0)">
-            <i class="fa fa-fw fa-times"></i>
-          </a>
-          <!-- END Close Sidebar -->
-        </div>
-        <!-- END Extra -->
-      </div>
-      <!-- END Side Header -->
-
-      <!-- Sidebar Scrolling -->
+        <!-- Sidebar Scrolling -->
       <div class="js-sidebar-scroll">
+
+          <!-- Side Header -->
+          <div class="content-header">
+
+              <!-- Side Header -->
+              <div class="side-header side-content bg-white-op">
+                  <!-- Logo -->
+                  <a class="fw-semibold text-dual" href="index.html">
+                      <span class="smini-visible"><img src="/favicons/carrot-16.png"></span>
+                      <span class="smini-hide fs-5 tracking-wider"><img src="/favicons/carrot-16.png" style="display:inline-block; margin-top:-3px; margin-right: 5px; vertical-align: middle">{{ config('app.name', 'Laravel') }}</span>
+                  </a>
+                  <!-- END Logo -->
+
+              </div>
+              <!-- END Side Header -->
+
+              <div>
+                  <!-- Close Sidebar, Visible only on mobile screens -->
+                  <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                  <a class="d-lg-none btn btn-sm btn-alt-secondary ms-1" data-toggle="layout" data-action="sidebar_close" href="javascript:void(0)">
+                      <i class="fa fa-fw fa-times"></i>
+                  </a>
+                  <!-- END Close Sidebar -->
+              </div>
+          </div>
+
         <!-- Side Navigation -->
         <div class="content-side">
           <ul class="nav-main">
-            <li class="nav-main-item">
-              <a class="nav-main-link{{ request()->is('dashboard') ? ' active' : '' }}" href="/dashboard">
-                <i class="nav-main-link-icon si si-cursor"></i>
-                <span class="nav-main-link-name">Dashboard</span>
-              </a>
-            </li>
-            <li class="nav-main-heading">Various</li>
-            <li class="nav-main-item{{ request()->is('pages/*') ? ' open' : '' }}">
-              <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="true" href="#">
-                <i class="nav-main-link-icon si si-bulb"></i>
-                <span class="nav-main-link-name">Examples</span>
-              </a>
-              <ul class="nav-main-submenu">
-                <li class="nav-main-item">
-                  <a class="nav-main-link{{ request()->is('pages/datatables') ? ' active' : '' }}" href="/pages/datatables">
-                    <span class="nav-main-link-name">DataTables</span>
+
+              <li class="nav-main-item">
+                  <a class="nav-main-link{{ request()->is('contacts.index') ? ' active' : '' }}" href="/contacts">
+                      <i class="nav-main-link-icon si si-cursor"></i>
+                      <span class="nav-main-link-name">Contactpersonen</span>
                   </a>
-                </li>
-                <li class="nav-main-item">
-                  <a class="nav-main-link{{ request()->is('pages/slick') ? ' active' : '' }}" href="/pages/slick">
-                    <span class="nav-main-link-name">Slick Slider</span>
-                  </a>
-                </li>
-                <li class="nav-main-item">
-                  <a class="nav-main-link{{ request()->is('pages/blank') ? ' active' : '' }}" href="/pages/blank">
-                    <span class="nav-main-link-name">Blank</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-main-heading">More</li>
-            <li class="nav-main-item open">
-              <a class="nav-main-link" href="/">
-                <i class="nav-main-link-icon si si-globe"></i>
-                <span class="nav-main-link-name">Landing</span>
-              </a>
-            </li>
+              </li>
+
           </ul>
         </div>
         <!-- END Side Navigation -->
