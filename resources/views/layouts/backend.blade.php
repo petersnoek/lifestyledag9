cd8472ed744f2c355775625ba630c50cda23999a
Dewi van der Velden <99063668@mydavinci.nl>
2022-05-23 11:50:05 +0200

    Account registratie + logout
    1. breeze getest
    2. user seeder aangemaakt met test account
    3. link voor logout toegevoegd
    4. naam van ingelogde user aan dashboard toegevoegd

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -321,14 +321,14 @@
           <div class="dropdown d-inline-block ms-2">
             <button type="button" class="btn btn-sm btn-alt-secondary d-flex align-items-center" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <img class="rounded-circle" src="{{ asset('media/avatars/avatar10.jpg') }}" alt="Header Avatar" style="width: 21px;">
-              <span class="d-none d-sm-inline-block ms-2">John</span>
+              <span class="d-none d-sm-inline-block ms-2">{{Auth::user()->name;}}</span>
               <i class="fa fa-fw fa-angle-down d-none d-sm-inline-block ms-1 mt-1"></i>
             </button>
             <div class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0" aria-labelledby="page-header-user-dropdown">
               <div class="p-3 text-center bg-body-light border-bottom rounded-top">
                 <img class="img-avatar img-avatar48 img-avatar-thumb" src="{{ asset('media/avatars/avatar10.jpg') }}" alt="">
-                <p class="mt-2 mb-0 fw-medium">John Smith</p>
-                <p class="mb-0 text-muted fs-sm fw-medium">Web Developer</p>
+                <p class="mt-2 mb-0 fw-medium">{{Auth::user()->name;}}</p>
+                <p class="mb-0 text-muted fs-sm fw-medium">Student</p>
               </div>
               <div class="p-2">
                 <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
@@ -345,12 +345,13 @@
               </div>
               <div role="separator" class="dropdown-divider m-0"></div>
               <div class="p-2">
-                <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                  <span class="fs-sm fw-medium">Lock Account</span>
-                </a>
-                <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
+                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown-item d-flex align-items-center justify-content-between">
                   <span class="fs-sm fw-medium">Log Out</span>
                 </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
               </div>
             </div>
           </div>
