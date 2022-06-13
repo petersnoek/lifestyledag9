83bbdd54a074a17d55e8ec7eb7983558390d9cbe
Dewi van der Velden <dewi120304@hotmail.com>
2022-06-13 12:55:58 +0200

    Mail template uitgebreid

--- resources/views/emails/uitnodiging-mail.blade.php
+++ resources/views/emails/uitnodiging-mail.blade.php
@@ -1,22 +1,11 @@
 @component('mail::message')
 Beste {{ $user->name }},
 Wij organiseren weer een Lifestyledag op {{ $date }} in Gorinchem.
-Wilt u meehelpen? Vul dan dit formulier in.
+Wilt u meehelpen? Klik op de onderstaande link om de uitnodiging te accepteren.
 
-<!-- Placeholder voor activiteit -->
-<p><label for="Activiteit">Activiteit:</label></p>
-  <textarea id="Activiteit" name="Activiteit" rows="1" cols="20"></textarea>
-<br>
-
-<!-- Placeholder voor beschrijving -->
-<p><label for="Beschrijving">Beschrijving:</label></p>
-  <textarea id="Beschrijving" name="Beschrijving" rows="4" cols="50"></textarea>
-<br>
-
-<!-- Placeholder voor capaciteit -->
-<p><label for="Capaciteit">Capaciteit:</label></p>
-  <textarea id="Capaciteit" name="Capaciteit" rows="1" cols="20"></textarea>
-<br>
+@component('mail::button', ['url' => 'http://lifestyledag9.uitnodiging', 'color' => 'success'])
+Uitnodiging Lifestyledag
+@endcomponent
 
 {{ config('app.name') }}
 @endcomponent
---
+++ resources/views/uitnodiging.blade.php
@@ -0,0 +1,30 @@
+<form>
+  <p>Voor het organiseren van de Lifestyledag hebben we wat informatie nodig over uw activiteit.</p>
+
+  <!-- Placeholder voor organisator -->
+  <p><label for="organisator">Naam organisator:</label></p>
+    <textarea id="organisator" name="organisator" rows="1" cols="20"></textarea>
+  <br>
+
+  <!-- Placeholder voor bedrijf -->
+  <p><label for="bedrijf">Naam bedrijf:</label></p>
+   <textarea id="bedrijf" name="bedrijf" rows="1" cols="20"></textarea>
+  <br>
+
+  <!-- Placeholder voor activiteit -->
+  <p><label for="Activiteit">Activiteit:</label></p>
+    <textarea id="Activiteit" name="Activiteit" rows="1" cols="20"></textarea>
+  <br>
+
+  <!-- Placeholder voor beschrijving -->
+  <p><label for="Beschrijving">Beschrijving:</label></p>
+    <textarea id="Beschrijving" name="Beschrijving" rows="4" cols="50"></textarea>
+  <br>
+
+  <!-- Placeholder voor capaciteit -->
+  <p><label for="Capaciteit">Capaciteit:</label></p>
+    <input type="Capaciteit" min="1" max="30">
+  <br>
+
+  <input type="submit" value="Formulier versturen">
+</form>
