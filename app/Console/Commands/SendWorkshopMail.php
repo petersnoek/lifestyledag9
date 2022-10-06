d5819d974e100e7bd9777b50339892421312cfd8
Dewi van der Velden <dewi120304@hotmail.com>
2022-10-06 14:24:52 +0200

    V2.1
    - query aangepast voor het ophalen van gegevens
    - begin gemaakt aan ophalen van de juiste workshophouder
    - begin gemaakt aan het versturen nadat de inschrijving voorbij is

--- app/Console/Commands/SendWorkshopMail.php
+++ app/Console/Commands/SendWorkshopMail.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
@@ -12,34 +11,59 @@ use App\Mail\WorkshopMail;
 class SendWorkshopMail extends Command
 {
     protected $signature = 'info:day';
-
     protected $description = 'Send an email to the workshop owner with the participants of the rounds';
 
     public function handle()
     {
-        $endDate = date('2022-09-04 09:00:00');
-        $currentDate = Carbon::now();
+        $getUser = DB::select("SELECT executed_by FROM `activities` WHERE id = 3");
+        $user = implode(" ", $getUser);
+        $workshophouders = $user;
 
-        if($currentDate > $endDate){  
-            $mailInfo = DB::select( 
-                "SELECT DISTINCT enlistments.round_id, enlistments.event_id, users.name, activities.name as activity, activities.executed_by as workshophouder, eventrounds.start_time, eventrounds.end_time
-                FROM enlistments 
-                INNER JOIN users
-                ON enlistments.user_id=users.id
-                INNER JOIN activities
-                ON enlistments.activity_id=activities.id
-                INNER JOIN activities as activities2
-                ON enlistments.activity_id=activities.id
-                INNER JOIN eventrounds
-                ON enlistments.round_id=eventrounds.id
-                WHERE activities.executed_by = 'Marc & Peter'"
-              );
+        $mailInfo = DB::select( 
+            "SELECT DISTINCT enlistments.round_id, enlistments.event_id, users.name, activities.name as activity, activities.executed_by as workshophouder, eventrounds.start_time, eventrounds.end_time
+            FROM enlistments 
+            INNER JOIN users
+            ON enlistments.user_id=users.id
+            INNER JOIN activities
+            ON enlistments.activity_id=activities.id
+            INNER JOIN activities as activities2
+            ON enlistments.activity_id=activities.id
+            INNER JOIN eventrounds
+            ON enlistments.round_id=eventrounds.id
+            WHERE activities.executed_by = $workshophouders"
+        );
 
-            $users = User::all();
-            foreach ($users as $user) {
-                    Mail::to($user->email)->send(new WorkshopMail($mailInfo));
-                };
-            }
-            $this->info('worksop information sent to workshop owner');
+        foreach (str_split($workshophouders) as $values) {
+            foreach ($values as $workshophouder) {
+                Mail::to($workshophouder->email)->send(new WorkshopMail($mailInfo));
+            };
         }
-    }
+        $this->info('worksop information sent to workshop owner');
+
+
+
+
+
+
+
+
+        // $mailInfo = DB::select( 
+        //     "SELECT DISTINCT enlistments.round_id, enlistments.event_id, users.name, activities.name as activity, activities.executed_by as workshophouder, eventrounds.start_time, eventrounds.end_time
+        //     FROM enlistments 
+        //     INNER JOIN users
+        //     ON enlistments.user_id=users.id
+        //     INNER JOIN activities
+        //     ON enlistments.activity_id=activities.id
+        //     INNER JOIN activities as activities2
+        //     ON enlistments.activity_id=activities.id
+        //     INNER JOIN eventrounds
+        //     ON enlistments.round_id=eventrounds.id
+        //     WHERE activities.executed_by = 'Marc & Peter'"
+        //     );
+
+        // $users = User::all();
+        // foreach ($users as $user) {
+        //     Mail::to($user->email)->send(new WorkshopMail($mailInfo));
+        // };
+    }   
+}
--- app/Http/Kernel2.php
+++ app/Http/Kernel2.php
@@ -3,6 +3,8 @@
 namespace App\Console;
  
 use Illuminate\Console\Scheduling\Schedule;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
  
 class Kernel extends ConsoleKernel
@@ -13,9 +15,22 @@ class Kernel extends ConsoleKernel
  
     protected function schedule(Schedule $schedule)
     {
-        // schedule command
-        $schedule->command('info:day')
-        ->twiceDaily(8, 18);
+        // Schedule command
+        $getDate = DB::select("SELECT starts_at  FROM `events` WHERE id = 1");
+        $date = implode(" ", $getDate);
+        var_dump($date);
+        $endDate = Carbon::create($date)->subDays(1);
+        var_dump($endDate);
+        $currentDate = Carbon::now();
+
+        if($currentDate == $endDate){ 
+            $schedule->command('info:day')
+            ->$endDate(8);
+        }
+
+        // Schedule command
+        // $schedule->command('info:day')
+        //     ->daily(8);
 
         // Laravel also allows you to schedule shell commands 
         // so that you could issue commands to the operating system and external applications.
@@ -27,7 +42,6 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
- 
         require base_path('routes/console.php');
     }
 }
