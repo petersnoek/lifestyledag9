f9f7a5fcc8b3e584434173ad0bd0b4037f9dcd7b
Dewi van der Velden <dewi120304@hotmail.com>
2022-10-13 11:11:32 +0200

    V2.5
    - data wordt via model opgehaald
    - mail wordt verstuurd zonder data

--- app/Console/Commands/SendWorkshopMail.php
+++ app/Console/Commands/SendWorkshopMail.php
@@ -2,14 +2,14 @@
 
 namespace App\Console\Commands;
 
-use App\Models\User;
 use Carbon\Carbon;
+use App\Models\User;
 use App\Models\Enlistment;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\WorkshopMail;
-use Mockery\Undefined;
+use App\Models\Activity;
 
 class SendWorkshopMail extends Command
 {
@@ -24,83 +24,38 @@ class SendWorkshopMail extends Command
 
         // Haal de workshophouders op waarvan de inschrijfdatum verlopen is
         $getUser = DB::select(
-            "SELECT activities.owner_user_id, events.enlist_stops_at  
+            "SELECT DISTINCT activities.owner_user_id, events.enlist_stops_at, activities.id  
             FROM `activities` 
+            INNER JOIN `users`
+            ON activities.owner_user_id=users.id
             INNER JOIN `events` 
-            WHERE events.enlist_stops_at = '2022-08-31 08:00:00' AND activities.owner_user_id = '2'" // $dateNow
+            WHERE events.enlist_stops_at = '2022-08-31 08:00:00' AND users.workshop_owner = '1'" // $dateNow
         ); 
-
         // Loop door de opgehaalde workshophouders heen
         $number = 0;
         foreach ($getUser as $key => $value) {
             if(isset($getUser) == true){
-                $number ++;
-                $user = $getUser[$number]->owner_user_id;
-                $workshophouders = $user;
-                var_dump($workshophouders);
+                $number++;
+                $user = $value->owner_user_id;
+                $activity = $value->id;
+                $workshophouder = $user;
 
                 // Haal data op per workshophouder voor de mail
-                $mailInfo = Enlistment::whereName($workshophouders)->get();
+                $mailInfo = Enlistment::whereActivityId($activity)->get();
                 var_dump($mailInfo);
 
                 // Verstuur de mail met data naar de workshophouder
-                // foreach ($workshophouders as $workshophouder) {
-                //     Mail::to($workshophouder->email)->send(new WorkshopMail($mailInfo));
-                // }
+                $userInfo = DB::select(
+                    "SELECT DISTINCT activities.owner_user_id, users.name, users.email 
+                    FROM `activities` 
+                    INNER JOIN users 
+                    ON activities.owner_user_id = users.id
+                    WHERE activities.owner_user_id = $workshophouder"
+                );
+                Mail::to($userInfo[0]->email)->send(new WorkshopMail($mailInfo));
+                // Mail::to($userInfo[$number]->email)->send(new WorkshopMail($mailInfo));
             }
             $this->info('workshop information sent to workshop owner');
         }
     }   
 }
-
-
-
-
-// public function handle()
-// {
-//     // Haal de huidige datum op
-//     $currentDate = Carbon::now();
-//     $dateNow = $currentDate->format('Y-m-d H:i:s');
-
-//     // Haal de workshophouders op waarvan de inschrijfdatum verlopen is
-//     $getUser = DB::select(
-//         "SELECT activities.owner_user_id, events.enlist_stops_at  
-//         FROM `activities` 
-//         INNER JOIN `events` 
-//         WHERE events.enlist_stops_at = '2022-08-31 08:00:00' AND activities.owner_user_id = 'Dewi'" // $dateNow
-//     ); 
-
-//     $number = 0;
-//     foreach ($getUser as $key => $value) {
-//         if(!$getUser){
-//             $number ++;
-//             $user = $getUser[$number]->owner_user_id;
-//             $workshophouders = $user;
-
-//             // Haal de data van de activiteit op per workshophouder en haal de mail op van de workshophouder
-//             $mailInfo = DB::select( 
-//                 "SELECT DISTINCT contacts.email, enlistments.round_id, enlistments.event_id, users.name as student, activities.name as activity, activities.owner_user_id as workshophouder, eventrounds.start_time, eventrounds.end_time
-//                 FROM enlistments 
-//                 INNER JOIN users
-//                 ON enlistments.user_id=users.id
-//                 INNER JOIN activities
-//                 ON enlistments.activity_id=activities.id
-//                 INNER JOIN activities as activities2
-//                 ON enlistments.activity_id=activities.id
-//                 INNER JOIN eventrounds
-//                 ON enlistments.round_id=eventrounds.id
-//                 INNER JOIN contacts 
-//                 WHERE activities.owner_user_id = 'Dewi'" // $workshophouders
-//             );
-
-//             // Verstuur de mail met de opgehaalde data
-//             foreach (str_split($workshophouders) as $value) {
-//                 foreach ($value as $workshophouder) {
-//                     // Mail::to($workshophouder->email)->send(new WorkshopMail($mailInfo));
-//                     Mail::to('test@gmail.com')->send(new WorkshopMail($mailInfo));
-//                 };
-//             }
-//         }
-//         $this->info('workshop information sent to workshop owner');
-//     }
-// } 
--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -12,6 +12,11 @@ class Activity extends Model
 {
     use HasFactory;
 
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+
     public function event() {
         return $this->belongsTo(Event::class);
     }
