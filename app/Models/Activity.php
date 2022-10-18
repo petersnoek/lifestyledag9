4a08c593050f0950cfa0b25d199b15f36d917b08
Joshua <Doerak1aug@gmail.com>
2022-10-18 09:24:56 +0200

    V2

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -1,16 +1,16 @@
-<?php
+<?php 
 
-namespace App\Models;
+namespace App\Models; 
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use App\Models\Event;
-use App\Models\User;
-use App\Models\Enlistment;
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
+use Illuminate\Database\Eloquent\Model; 
+use App\Models\Event; 
+use App\Models\User; 
+use App\Models\Enlistment; 
 
-class Activity extends Model
-{
-    use HasFactory;
+class Activity extends Model 
+{ 
+    use HasFactory; 
 
     protected $fillable = [
         'name',
@@ -18,34 +18,53 @@ class Activity extends Model
         'event_id',
         'owner_user_id',
     ];
+    public function event() { 
+        return $this->belongsTo(Event::class); 
+    } 
 
-    public function owner() {
-        return $this->belongsTo(User::class); //!!!! idk how to do this !!!!
-    }
-    public function event() {
-        return $this->belongsTo(Event::class);
-    }
+    public function enlistments() { 
+        return $this->hasMany(Enlistment::class); 
+    } 
 
-    public function enlistments() {
-        return $this->hasMany(Enlistment::class);
-    }
+    public function rounds() { 
+        // return 'test'; 
+        return $this->hasMany(ActivityRound::class); 
+    } 
 
-    function max_participants() {
-        return $this->hasMany(ActivityRound::class);
-    }
+    function max_participants() { 
+        return $this->hasMany(ActivityRound::class); 
+    } 
 
-    function max_participants_round($eventround_id) {
-        return $this->hasMany(ActivityRound::class)->where('eventround_id', $eventround_id);
-    }
+    function max_participants_round($eventround_id) { 
+        return $this->hasMany(ActivityRound::class)->where('eventround_id', $eventround_id); 
+    } 
 
-    function availability_message($eventround_id, $max_count) {
-        $enlistment_count = $this->enlistments()
-            ->where('round_id', $eventround_id)->count();
+    // function availability_message($eventround_id, $max_count) { 
+    //     $enlistment_count = $this->enlistments() 
+    //         ->where('round_id', $eventround_id)->count(); 
 
-        if ($enlistment_count >= $max_count) {
-            return 'vol';
-        } else {
-            return ($max_count - $enlistment_count) . ' plekken beschikbaar';
-        }
-    }
-}
+    //     if ($enlistment_count >= $max_count) { 
+    //         return 'vol'; 
+    //     } else { 
+    //         return ($max_count - $enlistment_count) . ' plekken beschikbaar'; 
+    //     } 
+    // } 
+
+    function availability_message($eventround_nr, $eventround_id) { 
+        $enlistment_count = $this->enlistments()->where('round_id', $eventround_nr)->count(); 
+        $round = $this->rounds()->where('eventround_id', $eventround_id)->first(); 
+        if($round == null) { 
+            $max_participants = 0; 
+        } else { 
+            $max_participants = $round->max_participants; 
+        } 
+
+        if ($max_participants <= 0) { 
+            return 'geen plaatsen beschikbaar'; 
+        } else if ($enlistment_count >= $max_participants) { 
+            return 'vol'; 
+        } else { 
+            return ($max_participants - $enlistment_count) . ' plekken beschikbaar'; 
+        } 
+    } 
+} 
