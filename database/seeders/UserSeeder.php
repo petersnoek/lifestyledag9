2f17a7bb892d6c7d6b8937ee79ea14aa36640e7d
bas verdoorn <99061684@mydavinci.nl>
2022-11-08 13:57:19 +0100

    V2.2.0 - test users aangemaakt

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -28,6 +28,12 @@ class UserSeeder extends Seeder
                 'email' => '99047256@mydavinci.nl',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$qgwRljCpvoHAPFGqB6sOgelHP1Qz9Ela0SH2MNW50SKczWbPYF78W',
+            ],
+            [
+                'name' => 'Student',
+                'email' => 'student@hotmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$5jnlp82C7RYyWNKNEYXzPOj3su8JcCmonxpaAzLXSE6VGQncZybDm'
             ]
         ];
 
